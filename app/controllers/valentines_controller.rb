dfffe8b20eb818b95fec65c53ee978cd68111037
Caleb Atwood <calebwilliamatwood@gmail.com>
2015-02-13 11:46:16 -0700

    Add heart icon

--- app/controllers/valentines_controller.rb
+++ app/controllers/valentines_controller.rb
@@ -1,6 +1,27 @@
 class ValentinesController < ApplicationController
 
   def index
+    @valentines = Valentine.all
+  end
+
+  def new
+    @valentine = Valentine.new
+  end
+
+  def create
+    @valentine = Valentine.new(valentine_params)
+      if @valentine.save
+        redirect_to root_url
+      else
+        render :new
+      end
+  end
+  
+
+  private
+
+  def valentine_params
+    params.require(:valentine).permit(:name, :message)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
 
   resources :users
+  resources :valentines
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'users#index'
+   root 'valentines#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
